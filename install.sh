3e001b497d41fdcb70d0fe8601623d79fbd1160a
ThotAlion <thomas.peyruse@gmail.com>
2018-08-02 11:54:48 +0200

    commit about nodejs installation on Raspbian

--- install.sh
+++ install.sh
@@ -6,12 +6,22 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 
 echo "----------------------------------"
-echo "Install Python 2.7 dev environment"
+echo "Install Python 2.7/git dev environment"
 sudo apt-get -y install python-dev python-pip ipython git
+git config --global credential.helper store
 
 echo "----------------------------------"
 echo "Install Pypot"
 sudo apt-get -y install libopenblas-dev
 sudo pip install pypot
+
+echo "----------------------------------"
+echo "Install NodeJs 8"
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install nodejs
+npm install socket.io
+npm install zeromq
+npm install express
+npm install ip
 
 
